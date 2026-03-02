42708fe91ec29d8ac28d26663159e7e0511e02ff
Julia Hong <hong0747@umn.edu>
2026-03-02 11:51:39 -0600

    Completed data import

--- R/week6.R
+++ R/week6.R
@@ -1,15 +1,15 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
-
+library(stringi)
+library(rebus.base)
 
 
 # Data Import
-
-
-
-
+citations <- stri_read_lines("../data/cites.txt")
+citations_txt <- str_subset(citations, pattern = ANY_CHAR, negate = FALSE)
+paste("The number of blank lines eliminated was", formatC(length(citations) - length(citations_txt)))
+paste("The average number of characters/citation was", formatC(mean(str_length(citations_txt))))
 
 # Data Cleaning
 
